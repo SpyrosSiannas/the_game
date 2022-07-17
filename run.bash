c921386542d31213caee1507ca6b09087892f289
Spyros Siannas <ssiannas@fb.com>
2022-07-17 19:46:00 +0300

    Add runner script

---
+++ run.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+python=python3
+pip=pip3
+
+if [ ! -d env ]; then
+    $python -m venv env
+    source env/bin/activate
+    $python -m pip install --upgrade pip
+    $pip install -r requirements.txt
+else
+    source env/bin/activate
+fi
+
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --ball)
+      GAME="examples/ball_example.py"
+      shift # past argument
+      shift # past value
+      ;;
+    -m|--main)
+      GAME="main.py"
+      shift # past argument
+      shift # past value
+      ;;
+    --default)
+      GAME="main.py"
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+$python $GAME
